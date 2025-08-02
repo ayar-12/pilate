b0f0d200b95023ec88e6f0d8fc9b80c224f65f66
Ayar <waadissa81@gmail.com>
2025-08-03 03:10:49 +0400

    Update userAuth.js

--- Backend/middleware/userAuth.js
+++ Backend/middleware/userAuth.js
@@ -3,16 +3,19 @@ import User from "../models/user.js";
 
 export const userAuth = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "No token" });
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "No token in header" });
+    }
 
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (!decoded?.id) return res.status(401).json({ message: "Invalid token" });
 
     const user = await User.findById(decoded.id).select("-password");
     if (!user) return res.status(401).json({ message: "User not found" });
 
-    req.user = user; 
+    req.user = user;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Unauthorized", error: err.message });

755a403c54b18a4f8677e6b71da7e18cd2f9169e
Ayar <waadissa81@gmail.com>
2025-08-03 04:08:30 +0400

    Update uploadController.js

--- Backend/controller/uploadController.js
+++ Backend/controller/uploadController.js
@@ -91,15 +91,20 @@ const uploadFiles = async (req, res) => {
         fs.unlinkSync(filePath);
       }
     }
-
-res.status(201).json({
-  success: true,
-  data: {
-    avatar: img?.secure_url,
-    video: vid?.secure_url,
-    pdf: pdf?.secure_url,
-  }
+const newUser = new userModel({
+  name: req.body.name,
+  avatar: img?.secure_url,
+  video: vid?.secure_url,
+  pdf: pdf?.secure_url,
+  cloudinary_id_img: img?.public_id,
+  cloudinary_id_vid: vid?.public_id,
+  cloudinary_id_pdf: pdf?.public_id,
 });
+
+await newUser.save();
+
+res.status(201).json({ success: true, data: newUser });
+
 
 
     await user.save();

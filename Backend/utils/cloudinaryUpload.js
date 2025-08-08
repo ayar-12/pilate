1d579b679e8b382a17f5de87a8fe6febf9a5a706
Ayar <waadissa81@gmail.com>
2025-08-09 03:23:06 +0400

    Update cloudinaryUpload.js

--- Backend/utils/cloudinaryUpload.js
+++ Backend/utils/cloudinaryUpload.js
@@ -1,1 +1,13 @@
 
+const { v2: cloudinary } = require('cloudinary');
+const streamifier = require('streamifier');
+
+module.exports = function uploadBufferToCloudinary(file, folder, resource_type) {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder, resource_type, overwrite: true },
+      (err, result) => err ? reject(err) : resolve(result)
+    );
+    streamifier.createReadStream(file.buffer).pipe(stream);
+  });
+};

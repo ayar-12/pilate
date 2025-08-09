b5e28d5344ddf30ac16ad92fe61f53df6093c58e
Ayar <waadissa81@gmail.com>
2025-08-09 05:54:45 +0400

    Update favorite.js

--- Backend/models/favorite.js
+++ Backend/models/favorite.js
@@ -1,8 +1,13 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-const favoriteSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  blog: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true },
-}, { timestamps: true });
+const favoriteSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    blog: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true },
+  },
+  { timestamps: true }
+);
+
+favoriteSchema.index({ user: 1, blog: 1 }, { unique: true });
 
 module.exports = mongoose.model('Favorite', favoriteSchema);
